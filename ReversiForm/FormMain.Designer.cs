d9863787869437ab4b10c19cf572f3d52f2ba65c
吉永　祐太 <yuta.yoshinaga@gmail.com>
2017-10-20 21:27:51 +0900

    first commit

--- ReversiForm/FormMain.Designer.cs
+++ ReversiForm/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReversiForm
 {
-    partial class Form1
+    partial class Reversi
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reversi));
+            this.SuspendLayout();
+            // 
+            // Reversi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Reversi";
+            this.Text = "Reversi";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- ReversiForm/FormMain.cs
+++ ReversiForm/FormMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ReversiForm
 {
-    public partial class Form1 : Form
+    public partial class Reversi : Form
     {
-        public Form1()
+        public Reversi()
         {
             InitializeComponent();
         }
